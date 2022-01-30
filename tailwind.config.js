3cae443f86c9f8d212655f5b72a696952d6f6aec
kitagawakeita <k.syarin610@gmail.com>
2022-01-30 19:05:45 +0900

    animationの追加

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,22 @@
 module.exports = {
   content: [],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        "fade-in":
+          "fade-in 1.2s cubic-bezier(0.390, 0.575, 0.565, 1.000)   both",
+      },
+      keyframes: {
+        "fade-in": {
+          "0%": {
+            opacity: "0",
+          },
+          to: {
+            opacity: "1",
+          },
+        },
+      },
+    },
   },
   plugins: [],
   purge: ["./src/**/*.{js,ts,jsx,tsx}"],
