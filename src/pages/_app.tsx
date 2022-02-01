a7d94f4cf5b4c19e0d6814c6ffa8ec986bcc4e36
kitagawakeita <k.syarin610@gmail.com>
2022-02-01 20:13:38 +0900

    recoilRootの削除

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,8 @@
 import "tailwindcss/tailwind.css";
-import { RecoilRoot } from "recoil";
 
 import type { AppProps } from "next/app";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return (
-    <RecoilRoot>
-      <Component {...pageProps} />
-    </RecoilRoot>
-  );
+  return <Component {...pageProps} />;
 }
 export default MyApp;
