dbfdb51606c77ebc0667d6cc29612e52891e14c6
kitagawakeita <k.syarin610@gmail.com>
2022-02-02 14:54:05 +0900

    update component

--- src/components/chat.tsx
+++ src/components/chat.tsx
@@ -1,15 +1,5 @@
 import { deleteText } from "src/hooks/deleteText";
 
-const d = new Date();
-
-const year = d.getFullYear();
-const month = d.getMonth() + 1;
-const date = d.getDate();
-const hour = d.getHours();
-const minute = d.getMinutes();
-
-const nowTime = `${year}年${month}月${date}日 ${hour}:${minute}`;
-
 type Props = {
   text: string;
   i: number;
@@ -17,28 +7,28 @@ type Props = {
 };
 
 export const Chat = (props: Props) => {
+  const d = new Date();
+
+  const year = d.getFullYear();
+  const month = d.getMonth() + 1;
+  const date = d.getDate();
+  const hour = d.getHours();
+  const minute = d.getMinutes();
+
+  const nowTime = `${year}年${month}月${date}日 ${hour}:${minute}`;
 
   return (
-    <div className="group flex relative py-4 mx-auto w-9/12 border-t animate-fade-in">
-      <div className="hidden group-hover:flex absolute -top-6 right-2 py-1 bg-white rounded-md border">
+    <div className="group relative py-4 mx-auto w-9/12 border-t animate-fade-in">
+      <p className="text-right text-sm text-gray-500">{nowTime}</p>
+      <div className="flex">
+        <p className="text-3xl font-bold mx-3">{props.i + 1}</p>
+        <p className="my-2 w-9/12">{props.text}</p>
         <svg
           xmlns="http://www.w3.org/2000/svg"
-          className="p-1 mx-1 w-8 h-8 hover:bg-gray-100 cursor-pointer"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth={1.2}
-            d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z"
-          />
-        </svg>
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          className="p-1 mx-1 w-8 h-8 hover:bg-gray-100 cursor-pointer"
-          onClick={() => {return props.delete(props.i)}}
+          className="p-1 my-1 hidden group-hover:block ml-auto w-8 h-8 hover:bg-gray-100 cursor-pointer border rounded-md"
+          onClick={() => {
+            return props.delete(props.i);
+          }}
           fill="none"
           viewBox="0 0 24 24"
           stroke="currentColor"
@@ -50,27 +40,7 @@ export const Chat = (props: Props) => {
             d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"
           />
         </svg>
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          className="p-1 mx-1 w-8 h-8 hover:bg-gray-100 cursor-pointer"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth={1.2}
-            d="M8 7v8a2 2 0 002 2h6M8 7V5a2 2 0 012-2h4.586a1 1 0 01.707.293l4.414 4.414a1 1 0 01.293.707V15a2 2 0 01-2 2h-2M8 7H6a2 2 0 00-2 2v10a2 2 0 002 2h8a2 2 0 002-2v-2"
-          />
-        </svg>
       </div>
-      <h3 className="mx-4 -mt-1 text-6xl font-bold">3</h3>
-      <div className="">
-        <p className="text-lg font-bold">あなた</p>
-        <p className="my-2">{props.text}</p>
-      </div>
-      <p className="ml-auto text-sm text-gray-500">{nowTime}</p>
     </div>
   );
 };
