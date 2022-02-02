15dc7c77e279f8682c8463d0303bd26c55cd1e5c
kitagawakeita <k.syarin610@gmail.com>
2022-02-02 19:34:26 +0900

    hooksの削除

--- src/hooks/addText.tsx
+++
@@ -1,8 +0,0 @@
-import { state } from "src/pages/index";
-
-export const addText = (text: string) => {
-  if (!text) {
-    return;
-  }
-  state.arr.push(text);
-};
--- src/hooks/deleteText.tsx
+++
@@ -1,5 +0,0 @@
-import { state } from "src/pages/index";
-
-export const deleteText = (i: number) => {
-  state.arr.splice(i, 1);
-};
